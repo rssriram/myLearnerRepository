e1ee2e6fd353692a5bea1d6626acc6873c852102
rssriram <learnersrs@gmail.com>
2016-09-03 21:21:48 -0700

    First Change

--- Learner/src/mylearner/HelloWorld.java
+++ Learner/src/mylearner/HelloWorld.java
@@ -15,6 +15,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		displayme();
+	}
+	
+	private static void displayme(){
+		System.out.println("Test");
 	}
 
 }
